38aeb59f4f876f6d1b54c046bf3376049ec635ad
PhoneWyne <85339083+PhoneWyne@users.noreply.github.com>
2025-02-24 14:47:37 +0500

    added functionality to add new contact info, working on filter

--- part2/exercises/phonebook/src/App.jsx
+++ part2/exercises/phonebook/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Person from "./components/Person";
+const App = () => {
+  // state to control persons displayed, array of objects
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
 
-function App() {
-  const [count, setCount] = useState(0)
+  // state to control name input field
+  const [newName, setNewName] = useState("");
+  // state to control number input field
+  const [newNumber, setNewNumber] = useState("");
+  // update name input field state
+  const handleNewName = (event) => {
+    console.log(`name input field: ${event.target.value}`);
+    setNewName(event.target.value);
+  };
+  // update number input field state state
+  const handleNewNumber = (event) => {
+    console.log(`number input field: ${event.target.value}`);
+    setNewNumber(event.target.value);
+  };
+
+  // handle adding new person
+  // 2 edge cases, preventing same name, and empty strings
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    // ensure empty string isnt being added
+    if (newName) {
+      console.log(`After submitting, new person name: ${newName}`);
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+      // this will hold undefined if new name does not exist already
+      const alreadyAdded = persons.find(
+        (person) => person.name === newPerson.name
+      );
+      if (!alreadyAdded) {
+        console.log("new name does not exist in storage");
+        // create copy of persons array and add newpersons obj to it
+        setPersons(persons.concat(newPerson));
+      } else {
+        alert(`${newName} does exist in storage, try again.`);
+      }
+
+      // reset input field
+      setNewName("");
+    } else {
+      alert("No name entered, enter a name then press add.");
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        filter shown with <input />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          // provide key to list component
+          <Person key={person.id} person={person} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
