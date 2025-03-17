dd334dd8c5883c9bb3a2558ed7390abe98b40382
PhoneWyne <85339083+PhoneWyne@users.noreply.github.com>
2025-03-17 14:17:56 +0500

    FSO part2, redid data to fetch from db.json instead of hard-coded

--- part2/exercises/phonebook/src/App.jsx
+++ part2/exercises/phonebook/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 // import Person from "./components/Person";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
-
 const App = () => {
   // state to control persons displayed, array of objects
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   // state to control name input field
   const [newName, setNewName] = useState("");
@@ -20,6 +16,20 @@ const App = () => {
   // state to control filter field
   const [filterName, setFilterName] = useState("");
 
+  const fetchPersons = async () => {
+    try{
+      const response = await axios.get('http://localhost:3001/persons')
+      if(response) {
+        setPersons(response.data);
+      }
+    } catch(error) {
+      console.error("Error in fetching data: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPersons();
+  }, []);
   // update name input field state
   const handleNewName = (event) => {
     console.log(`name input field: ${event.target.value}`);
