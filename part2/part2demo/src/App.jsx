d1a0610ae4ad8f642d0d3635a3b89c5863e1dc1b
PhoneWyne <85339083+PhoneWyne@users.noreply.github.com>
2025-09-21 23:01:45 +0500

    feat: pushed data

--- part2/part2demo/src/App.jsx
+++ part2/part2demo/src/App.jsx
@@ -1,16 +1,17 @@
-const App = (props) => {
-  const { notes } = props
+import Note from "./components/Note";
 
+const App = ({ notes }) => {
+  console.log(notes);
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => {
+          return <Note key={note.id} note={note} />;
+        })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
